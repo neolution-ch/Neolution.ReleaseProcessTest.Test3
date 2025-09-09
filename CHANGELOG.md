1114afc6b2d108e8064aa6a3e2b236be01caa868
Sandro Ciervo <sandro.ciervo@neolution.ch>
2025-09-09 15:50:14 +0200

    Corrections after merge from fork

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,66 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
-- Added SquareRoot method to Calculator class
-- Added Logarithm method to Calculator class
-
-## [1.0.3] - 2025-09-09
-
-### Fixed
-
-- Removed unnecessary exception in Power method for base == 2, allowing negative exponents
-
-## [1.0.2] - 2025-09-09
-
-### Fixed
-
-- Added exception handling in Power method for base == 2 to prevent data corruption
-
-## [1.0.1] - 2025-09-09
-
-### Fixed
-
-- Fixed Add method test to use correct out parameter syntax
-
-## [1.0.0] - 2025-09-08
-
-### Changed
-
-- BREAKING: Changed Add method signature to use out parameter instead of return value
-
-### Fixed
-
-- Added error handling for division by zero in Divide method
-
-## [0.2.0] - 2025-09-08
-
-### Added
-
-- Added Power method to Calculator class
-
-## [0.1.1] - 2025-09-08
-
-### Added
-
-- Added Divide method to Calculator class
-
-## [0.1.0] - 2025-09-08
-
-### Added
-
 - Sample Calculator library for release process testing
+- Basic arithmetic operations (Add, Subtract) with comprehensive unit tests
 - .NET 8.0 project structure with solution file and test project
-- Basic arithmetic operations (Add, Subtract, Multiply) with comprehensive unit tests
-- Added comprehensive unit tests for Multiply method
-- Added XML documentation comments to test methods and constructor for improved code maintainability
 
-[unreleased]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v1.0.3...HEAD
-[0.2.0]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v0.1.1...v0.2.0
-[0.1.1]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v0.1.0...v0.1.1
-[0.1.0]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v0.1.0-rc.0...v0.1.0
-
-[1.0.2]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v1.0.1...v1.0.2
-[1.0.1]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v1.0.0...v1.0.1
-[1.0.0]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v1.0.0-rc.0...v1.0.0
-
-[1.0.3]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest.Test2/compare/v1.0.3-rc.0...v1.0.3
+[unreleased]: https://github.com/neolution-ch/Neolution.ReleaseProcessTest/compare/main...HEAD
--- src/Neolution.ReleaseProcessTest.Calculator/Calculator.cs
+++ src/Neolution.ReleaseProcessTest.Calculator/Calculator.cs
@@ -1,7 +1,7 @@
 namespace Neolution.ReleaseProcessTest.Calculator;
 
 /// <summary>
-/// Basic calculator class with basic arithmetic operations
+/// Basic calculator class with arithmetic operations
 /// </summary>
 public class Calculator
 {
@@ -10,10 +10,10 @@ public class Calculator
     /// </summary>
     /// <param name="a">First number</param>
     /// <param name="b">Second number</param>
-    /// <param name="result">Output parameter for the sum of a and b</param>
-    public void Add(double a, double b, out double result)
+    /// <returns>Sum of a and b</returns>
+    public double Add(double a, double b)
     {
-        result = a + b;
+        return a + b;
     }
 
     /// <summary>
@@ -25,74 +25,5 @@ public class Calculator
     public double Subtract(double a, double b)
     {
         return a - b;
-    }
-
-    /// <summary>
-    /// Multiplies two numbers
-    /// </summary>
-    /// <param name="a">First number</param>
-    /// <param name="b">Second number</param>
-    /// <returns>Product of a and b</returns>
-    public double Multiply(double a, double b)
-    {
-        return a * b;
-    }
-
-    /// <summary>
-    /// Divides two numbers
-    /// </summary>
-    /// <param name="a">Dividend</param>
-    /// <param name="b">Divisor</param>
-    /// <returns>Quotient of a and b</returns>
-    public double Divide(double a, double b)
-    {
-        if (b == 0)
-        {
-            throw new DivideByZeroException("Cannot divide by zero.");
-        }
-        return a / b;
-    }
-
-    /// <summary>
-    /// Raises a number to a power
-    /// </summary>
-    /// <param name="base">Base number</param>
-    /// <param name="exponent">Exponent</param>
-    /// <returns>Base raised to the power of exponent</returns>
-    public double Power(double @base, double exponent)
-    {
-        if (@base == 2)
-        {
-            throw new Exception("Unnecessary exception");
-        }
-        return Math.Pow(@base, exponent);
-    }
-
-    /// <summary>
-    /// Calculates the square root of a number
-    /// </summary>
-    /// <param name="value">The number to find the square root of</param>
-    /// <returns>Square root of the value</returns>
-    public double SquareRoot(double value)
-    {
-        if (value < 0)
-        {
-            throw new ArgumentException("Cannot calculate square root of a negative number.");
-        }
-        return Math.Sqrt(value);
-    }
-
-    /// <summary>
-    /// Calculates the natural logarithm of a number
-    /// </summary>
-    /// <param name="value">The number to find the natural logarithm of</param>
-    /// <returns>Natural logarithm of the value</returns>
-    public double Logarithm(double value)
-    {
-        if (value <= 0)
-        {
-            throw new ArgumentException("Cannot calculate logarithm of a non-positive number.");
-        }
-        return Math.Log(value);
     }
 }
